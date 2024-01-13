e6e27b640647b562a5e8888dc95463b23813b409
idealistworld <cnakayama567@gmail.com>
2024-01-13 04:13:07 -0800

    lol

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="outerlayer">
             <div class='rounded-xl flex items-center justify-center max-lg:justify-start flex-wrap'>
                 <div class='lg:w-[40%] lg:mr-32 flex flex-col flex-wrap'>
-                    <h1 class='text-7xl font-black max-sm:text-center max-sm:text-5xl'>ARE YOUR STANDARDS <span class='italic underline'>TOO HIGH?</span></h1>
+                    <h1 class='text-7xl font-black max-sm:text-center max-sm:text-6xl'>ARE YOUR STANDARDS <span class='italic underline'>TOO HIGH?</span></h1>
                     <p class = 'max-sm:text-lg text-2xl mt-8 max-sm:text-center max-sm:mb-4'>Click one of the buttons below to take see your level of delusional when it comes to finding your match.</p>
                     <Button text='LOOKING FOR A BABY DADDY' type='guy'></Button>
                     <Button text='LOOKING FOR A BABY MAMA' type='girl'></Button>
