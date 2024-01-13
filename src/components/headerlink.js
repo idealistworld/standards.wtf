3addaebab8c8244831e4216d6bf7e678369bd454
idealistworld <cnakayama567@gmail.com>
2024-01-13 03:48:26 -0800

    lol

--- src/components/headerlink.js
+++ src/components/headerlink.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
 
 export default function Headerlink(props) {
-    return (<Link to={props.route} class = 'text-2xl font-black hover:bg-white px-2'>{props.title}</Link>);
+    return (<Link to={props.route} class = 'text-2xl font-black bg-white px-3 mx-3 hover:scale-110'>{props.title}</Link>);
 }
